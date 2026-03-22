b7ca38ec9db87aa24ae38c9a08b0048a21207255
VeronicaJimenez639 <va.jimenezm@uea.edu.ec>
2026-03-22 12:10:15 -0500

    Se creó la clase VisitaServicio y se preparó la lista interna de visitantes

--- visitas_app/servicios/visita_servicio.py
+++ visitas_app/servicios/visita_servicio.py
@@ -1,3 +1,9 @@
 from typing import List, Optional
 from modelos.visitante import Visitante
 
+class VisitaServicio:
+    def __init__(self):
+        # Aquí se guarda la lista de visitantes de forma interna.
+        # El guion bajo indica que no debe manipularse directamente desde fuera.
+        self._lista_visitantes: List[Visitante] = []
+
